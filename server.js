59df1b789cec0fdd18a4e6eedb172a5bb4b278d6
Christian Spiekermann <chrisspi94@gmail.com>
2026-08-20 11:30:01 +0200

    Server update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
 
-app.get("/", (_, res) => res.send("Hello from Dockerized Node!"));
+app.get("/", (_, res) => res.send("Hello from Dockerized!"));
+app.get("/test", (_, res) => res.send("Test!"));
 app.listen(3000, () => console.log("Server läuft auf Port 3000"));
